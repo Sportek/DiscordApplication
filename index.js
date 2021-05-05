5f9e4e4993972c1c8a47fd6412d6a6ff9caa5556
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-05-05 22:34:54 +0200

    :construction: ts-node → ts-node-dev

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@discord-factory/core");
-core_1.Factory.getInstance().init().then(() => {
-    console.log(core_1.Factory.getInstance().$container.events);
-});
