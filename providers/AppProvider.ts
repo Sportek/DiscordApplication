784f620eee0b02e9f668e8084c8218310b77102b
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-05-23 20:18:14 +0200

    :construction: Move module-aliases

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -1,8 +1,10 @@
 import { Provider } from '@discord-factory/core'
+import Motd from '@discord-factory/motd'
 
 export default class AppProvider implements Provider {
   public async boot(): Promise<void> {
     // Your code here
+    await new Motd().start()
   }
 
   public async loadFile(file): Promise<void> {
