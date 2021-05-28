b7738561f5f09775ff70b200ff53faff4e46a2d4
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-05-28 21:56:08 +0200

    :construction: Update dependencies and provider context file

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -1,4 +1,4 @@
-import { Provider } from '@discord-factory/core'
+import { Context, Provider } from '@discord-factory/core'
 import Motd from '@discord-factory/motd'
 
 export default class AppProvider implements Provider {
@@ -7,12 +7,11 @@ export default class AppProvider implements Provider {
     await new Motd().start()
   }
 
-  public async loadFile(file): Promise<void> {
+  public async loadFile(context: Context): Promise<void> {
     // Your code here
   }
 
   public async ready(): Promise<void> {
     // Your code here
-
   }
 }
