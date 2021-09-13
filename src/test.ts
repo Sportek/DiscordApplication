1583bd81fe45c8e06b7ed6cf956cc1b1aab80ab9
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-09-13 23:58:01 +0200

    :construction: Work in progress

--- src/test.ts
+++
@@ -1,8 +0,0 @@
-import { Event, BaseEvent } from 'ioc:factory/Core/Event'
-import { Message } from 'ioc:factory/Discord/Event'
-
-@Event('messageCreate')
-export default class MyEvent extends BaseEvent {
-  public async run (message: Message) {
-  }
-}
--- src/testHook.ts
+++
@@ -1,8 +0,0 @@
-import { Hook, BaseHook } from 'ioc:factory/Core/Hook'
-import { Client } from 'discord.js'
-
-@Hook('application::ok')
-export default class MyHook extends BaseHook {
-  public async run (client: Client) {
-  }
-}
