ceb06144cbcca2344208336daaaad6a8dd8bbbe2
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-09-14 14:41:47 +0200

    :construction: Work in progress

--- ace.ts
+++ ace.ts
@@ -2,13 +2,13 @@ import 'module-alias'
 import { Ignitor } from '@discord-factory/core-next'
 
 (async () => {
-  const [,, prefix] = process.argv
+  const [,, prefix, ...params] = process.argv
   const ignitor = new Ignitor()
   const init = await ignitor.createCommand()
 
   if (init) {
     const command = ignitor.container.cli.get(prefix)
-    command.run()
+    command.run(...params)
   }
-  process.exit()
+  // process.exit()
 })()
