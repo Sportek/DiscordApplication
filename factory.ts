5040d70789b43868c6346c8c1ae9ef138f731472
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-10-09 18:30:27 +0200

    :sparkles: Upgrade packages version

--- factory.ts
+++ factory.ts
@@ -2,10 +2,6 @@ import 'module-alias'
 import { Ignitor } from '@discord-factory/core-next'
 
 (async () => {
-  const [,, prefix, ...params] = process.argv
   const ignitor = new Ignitor()
-  await ignitor.createCommand()
-
-  const command = ignitor.container.cli.get(prefix)
-  await command.run(...params)
+  await ignitor.exec()
 })()
